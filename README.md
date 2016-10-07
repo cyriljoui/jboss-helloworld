1d4daf5f82171322697ee2c0bb07afde4e93ade9
michir <mahieddine.ichir@docapost.fr>
2016-10-07 10:37:37 +0200

    The first JSP we application

---
+++ README.md
@@ -0,0 +1,16 @@
+# JEE Tutorial on Wildfly
+
+# JSP web application
+
+## Wildfly
+* download / install the wildfly AS (8+)
+* start the wildfly server
+
+## Application
+
+### check configuration
+* clone this project 
+* deploy the application on wildfly using `mvn wildfly:deploy`
+
+### excercise 
+modify the _HelloServlet_ to set in your name and redeploy.
---
+++ src/main/java/com/cjo/jee/HelloWorldServlet.java
@@ -0,0 +1,24 @@
+package com.cjo.jee;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/hello")
+public class HelloWorldServlet extends HttpServlet {
+
+	/**
+	 * UID.
+	 */
+	private static final long serialVersionUID = 2563570703208835685L;
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setAttribute("user", "John Doe");
+		request.getRequestDispatcher("/WEB-INF/jsp/hello.jsp").forward(request, response);
+	}
+}
